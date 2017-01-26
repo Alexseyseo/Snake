bfec7dd3d9d8fc602516264de2c984960083b573
Aleksey Suprun <Alexsey.seo@gmail.com>
2017-01-26 23:54:05 +0300

    lesson7
    построили вертикальную и горизонтальную линии через отдельные классы

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> vList;
+
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            vList = new List<Point>();
+            for(int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                vList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in vList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
